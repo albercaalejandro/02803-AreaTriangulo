80cc530faabb7ac38dc6388c1e9a69793234cde8
Xtek <Xtek@Pc-Nasa>
2019-10-28 17:38:16 +0100

    :metal: Commit resubido

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package areatriangulo;
+package org.japo.java.main;
 
 import java.util.Scanner;
 
